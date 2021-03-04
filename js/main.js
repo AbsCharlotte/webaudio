386dd3a7ac98c3804e7588599c23782050b3d97e
AbsCharlotte <charlotte.truchet@univ-nantes.fr>
2021-03-04 11:40:42 +0100

    Update main.js

--- js/main.js
+++ js/main.js
@@ -22,7 +22,7 @@ function NotesGenerator(notes) {
             _this.playNote();
           else
             clearInterval(_this.interval);
-        }, 1000);
+        }, 500);
     }
     this.playNote = function() {
         const oscillator = context.createOscillator();
